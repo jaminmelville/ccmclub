3e99cd3f91d3420d756f1216da10608ac4ad74ed
Ben Melville <benmelville87@gmail.com>
2019-11-03 12:23:08 +1000

    WIP.

--- README.md
+++ README.md
@@ -2,11 +2,26 @@
 
 ## Setup
 
-* edit .env file.
+* `cp .env.dist .env`
 * run `docker-compose up -d`
+
+### Install wordpress site.
+`docker-compose run cli core install --url="localhost"  --title="CCMC" --admin_user="admin" --admin_password="password" --skip-email --admin_email="benmelville87@gmail.com"`
+
+### Install wordpress plugins.
+`docker-compose run cli plugin install advanced-custom-fields acf-field-date-time-picker acf-to-rest-api wordpress-seo --activate`
+
+### Install wordpress plugins.
+`docker-compose run cli rewrite structure '/%postname%'`
+
+### Make theme available.
+`sudo cp -r theme web/wp-content/themes/ccmclub`
+
+### Active ccmclub wordpress theme.
+`docker-compose run cli theme activate ccmclub`
+
 * edit siteurl and home to correct URLs
 * enable theme
-* add s3 credentials
 * enable advanced seo settings (/wp-admin/admin.php?page=wpseo_dashboard#top#features)
 * add seo fields for social etc.
 * add content
--- theme/scripts/api.php
+++ theme/scripts/api.php
@@ -1,37 +1,4 @@
 <?php
-  require_once __DIR__ . '/../vendor/autoload.php'; // change path as needed
-
-  function fetchFacebookAlbum($data) {
-    $images = get_transient('photos' . $data['id']);
-    if ($images) {
-      return $images;
-    }
-
-    $fb = new \Facebook\Facebook([
-      'app_id' => get_option('ccmc_fb_app_id'),
-      'app_secret' => get_option('ccmc_fb_app_secret'),
-      'default_graph_version' => 'v2.11',
-    ]);
-
-    try {
-      $response = $fb->get(
-        '/' . $data['id'] . '/photos?fields=images&limit=100',
-        '1849712832007043|41078426cb6fa04dc70813f7c5c1bbaf'
-      );
-    } catch(Exception $e) {
-      echo 'Graph returned an error: ' . $e->getMessage();
-    }
-
-    $images = [];
-    $edge = $response->getGraphEdge();
-    do {
-      foreach ($edge as $node) {
-        $images[] = $node->asArray();
-      }
-    } while ($edge = $fb->next($edge));
-    set_transient('photos' . $data['id'], $images, 60 * 60 * 24 * 7);
-    return $images;
-  }
 
   function cccmcApi() {
     $settings = [
@@ -45,10 +12,6 @@
   }
 
   add_action('rest_api_init', function() {
-    register_rest_route('fb/v1', '/album/(?P<id>\d+)', [
-      'methods' => 'GET',
-      'callback' => 'fetchFacebookAlbum',
-    ]);
     register_rest_route('ccmc/v1', '/settings', [
       'methods' => 'GET',
       'callback' => 'cccmcApi',
